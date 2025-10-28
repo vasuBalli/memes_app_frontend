2009b023fb6930e65327252ec717e356e1ccb96d
vasuBalli <vasuballi815@gmail>
2025-10-28 19:27:54 +0530

    testing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,7 +33,7 @@ export function Header() {
                   Memeverse
                 </h1>
                 <p className="text-[10px] text-zinc-500 dark:text-zinc-400 -mt-1">
-                  The Internet's Best
+                  The Internet's Besttt
                 </p>
               </div>
             </div>
